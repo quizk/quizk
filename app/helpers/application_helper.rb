de4c742c28bd810db270fb1fe3ee8d4d54f57c60
Anh Tran Tuan <yorkittran@gmail.com>
2022-05-29 21:30:46 +0700

    feat: add content column with truncate text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
+  TRUNCATE_LENGTH = 50
+
   def render_turbo_stream_flash
     turbo_stream.prepend 'flash', partial: 'layouts/ops/flash'
+  end
+
+  def truncate_actiontext(text, length = TRUNCATE_LENGTH)
+    text.to_plain_text.truncate(length)
   end
 end
