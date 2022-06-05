3bbfb6873adc117f4eb08eb6b7833587724a5ad7
Anh Tran Tuan <yorkittran@gmail.com>
2022-06-06 02:16:29 +0700

    works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   COLORS = %w[primary success info warning danger dark]
 
   def render_turbo_stream_flash
-    turbo_stream.prepend 'flash', partial: 'layouts/ops/flash'
+    turbo_stream.append 'flash', partial: 'layouts/ops/flash'
   end
 
   def truncate_actiontext(text, length = TRUNCATE_LENGTH)
